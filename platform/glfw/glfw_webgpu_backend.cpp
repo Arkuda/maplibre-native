28c57fc244bc8eed649f3cd6a6efb4451b8c377e
Arkuda <arkuda@users.noreply.github.com>
2026-08-01 12:14:52 +0000

    chore: keep only Android and iOS platforms
    Remove linux, macos, node, qt, windows, glfw platform bindings and
    their CI/build references. This fork targets Android and iOS only.

--- platform/glfw/glfw_webgpu_backend.cpp
+++
@@ -1,4 +0,0 @@
-#ifndef __APPLE__
-#define GLFW_WEBGPU_BACKEND_IMPLEMENTATION
-#include "glfw_webgpu_backend_impl.ipp"
-#endif
--- platform/glfw/glfw_webgpu_backend.mm
+++
@@ -1,2 +0,0 @@
-#define GLFW_WEBGPU_BACKEND_IMPLEMENTATION
-#include "glfw_webgpu_backend_impl.ipp"
